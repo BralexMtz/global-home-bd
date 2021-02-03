97b46b219f2e89146a94861c8fea1dc607de7bee
Bralex Martinez <brayanlexis.p6@gmail.com>
2021-02-03 13:57:25 -0600

    1 vista y algunas inserciones

---
+++ Scripts/s-08-vistas.sql
@@ -0,0 +1,9 @@
+--@Autor: Parada Pérez Jesús Bryan, Brayan Alexis Martinez Vazquez
+--@Fecha creación: 31/01/2021
+--@Descripción: Creación de vistas para el proyecto de BD
+
+create or replace view ventas(
+  duenio, ventas_totales_vacacionales, vivienda_id, direccion, cantidad_max, porcentaje
+) as select v.duenio, 
+     from 
+create or replace view 
